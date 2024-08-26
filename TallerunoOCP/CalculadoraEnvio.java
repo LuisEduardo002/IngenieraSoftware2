8f773bb57af155e1d9b684ec4be4a1789f662abc
LuisEduardo002 <luise.garciav@autonoma.edu.co>
2024-08-26 16:22:29 -0500

    Feat: Mejora de botones en general

---
+++ TallerunoOCP/CalculadoraEnvio.java
@@ -0,0 +1,6 @@
+public class CalculadoraEnvio {
+    public double calcularCostoEnvio(Envio envio, double peso, double distancia) {
+        return envio.calcularCosto(peso, distancia);
+    }
+}
+
---
+++ TallerunoOCP/EnvioInternacional.java
@@ -0,0 +1,6 @@
+public class EnvioInternacional implements Envio {
+    @Override
+    public double calcularCosto(double peso, double distancia) {
+        return peso * 2.0 + distancia * 0.5;
+    }
+}
