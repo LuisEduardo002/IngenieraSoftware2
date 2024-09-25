f06ebe13fac390bcda8cdd573ad27d077cfed98b
LuisEduardo002 <luise.garciav@autonoma.edu.co>
2024-09-25 14:32:30 -0500

    Taller9

--- Taller9DRY/src/main.java
+++ Taller9DRY/src/main.java
@@ -36,23 +36,23 @@ public class main {
 
     public static void main(String[] args) {
         // Ejemplo de ejecución para invertirCadena
-       String cadena = "Hola Mundo";
+       String cadena = "El que quiero que me quiera";
         System.out.println("Cadena original: " + cadena);
        
      System.out.println("Cadena invertida: " + invertirCadena(cadena));
 
         // Ejemplo de ejecución para encontrarMayor
-        int[] numeros = {4, 8, 15, 16, 23, 42};
+        int[] numeros = {1, 8, 35, 123, 0, 31};
         System.out.println("El mayor número es: " + encontrarMayor(numeros));
 
         // Ejemplo de ejecución para emailValido
-        String email = "usuario@dominio.com";
+        String email = "elelpepe.com";
         System.out.println("Validación de email: " + email);
         emailValido(email);
 
         // Ejemplo de uso de la clase Biblioteca y Libro
-        Libro libro1 = new Libro("1984", "George Orwell", 1949);
-        Libro libro2 = new Libro("Cien anos de soledad", "Gabriel García Márquez", 1967);
+        Libro libro1 = new Libro("Guerra de ppopo", "elmaspro", 1949);
+        Libro libro2 = new Libro("Harrtpotrer", "elepep", 1967);
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.agregarLibro(libro1);
         biblioteca.agregarLibro(libro2);
